e365eab173eee9bce35b584f0524e55e83447e57
BLuikoja <133855086+BLuikoja@users.noreply.github.com>
2023-06-01 10:58:13 +0300

    Add files via upload

--- endatehtud.py
+++ endatehtud.py
@@ -1,5 +1,7 @@
 import random
 
+alg, lopp = 23, 26
+
 names = ['Luikoja', 'Lepasalu', 'Smurf', 'Toomla', 'Sipelgas']
 laps = 10
 filename = 'Result.txt'
@@ -11,53 +13,85 @@ fastest_lap = ['Unknown', 999]
 three_sectors = [['Unknown', 999], ['Unknown', 999], ['Unknown', 999]]
 sectors_data = []
 
+
 def random_sector_time(mini, maxi):
-    return random.uniform(mini, maxi)
+    thousandth = random.randint(0, 999) / 1000
+    return random.randint(mini, maxi) + thousandth
+
 
 def one_lap_time(mini, maxi, driver_name):
+    this_total = 0
     sectors_data.clear()
-    lap_time = 0
-    for _ in range(3):
-        sector_time = random_sector_time(mini, maxi)
-        sectors_data.append(sector_time)
-        if sector_time < three_sectors[_][1]:
-            three_sectors[_] = [driver_name, sector_time]
-        lap_time += sector_time
-    return lap_time
+    for z in range(3):
+        this_sector = random_sector_time(mini, maxi)
+        if this_sector < three_sectors[z][1]:
+            three_sectors[z][0] = driver_name
+            three_sectors[z][1] = this_sector
+        this_total += this_sector
+        sectors_data.append(this_sector)
+    return this_total
 
-def format_time(sec):
-    m, s = divmod(int(sec), 60)
+
+def is_fastest_lap(driver_name, fastest_data):
+    if driver_name == fastest_data[0]:
+        return sec2time(fastest_data[1])
+    else:
+        return ""
+
+
+def sec2time(sec, n_msec=3):
+    if hasattr(sec, '__len__'):
+        return [sec2time(s) for s in sec]
+    m, s = divmod(sec, 60)
     h, m = divmod(m, 60)
-    return f"{h:02d}:{m:02d}:{s:02d}"
+    d, h = divmod(h, 24)
+    if n_msec > 0:
+        pattern = '%%02d:%%02d:%%0%d.%df' % (n_msec + 3, n_msec)
+    else:
+        pattern = r'%02d:%02d:%02d'
+    if d == 0:
+        return pattern % (h, m, s)
+    return ('%d days, ' + pattern) % (d, h, m, s)
 
-with open(filename, 'w', encoding='utf-8') as f:
+
+if __name__ == '__main__':
+    f = open(filename, 'w', encoding='utf-8')
     f.write(file_header)
     for name in names:
         lap_times = 0
         errors = []
         for lap in range(laps):
+            error = False
             if random.randint(0, 9) == 2:
-                lap_time = one_lap_time(30, 90, 'Unknown')
+                lap_times += one_lap_time(30, 90, 'Unknown')
                 errors.append(lap + 1)
+                error = True
             else:
-                lap_time = one_lap_time(minimum, maximum, name)
-                if lap_time < fastest_lap[1]:
-                    fastest_lap = [name, lap_time]
-            lap_times += lap_time
-            sectors_data_str = ';'.join(map(str, [lap + 1, name, lap_time] + sectors_data + [int(lap_time != 0)]))
-            f.write(sectors_data_str + '\n')
+                this_lap = one_lap_time(minimum, maximum, name)
+                if this_lap < fastest_lap[1]:
+                    fastest_lap[0] = name
+                    fastest_lap[1] = this_lap
+                lap_times += this_lap
+            line = ';'.join([str(lap + 1), name, str(sum(sectors_data)), str(sectors_data[0]),
+                             str(sectors_data[1]), str(sectors_data[2]), str(error)])
+            f.write(line + '\n')
         results.append([name, lap_times, errors])
+    f.close()
 
-results.sort(key=lambda x: x[1])
+    results = sorted(results, key=lambda x: x[1])
+    print(results)
 
-for idx, (name, lap_times, errors) in enumerate(results):
-    difference = format_time(lap_times - results[0][1]) if idx > 0 else ""
-    fastest_lap_str = format_time(fastest_lap[1]) if name == fastest_lap[0] else ""
-    lap_times_str = format_time(lap_times)
-    print(name.ljust(10), lap_times_str, difference, errors, fastest_lap_str)
+    for idx, person in enumerate(results):
+        if idx > 0:
+            difference = sec2time(person[1] - results[0][1])
+            print(person[0].ljust(10), sec2time(person[1], 3),
+                  difference, person[2], is_fastest_lap(person[0], fastest_lap))
+        else:
+            print(person[0].ljust(10), sec2time(person[1], 3), person[2], is_fastest_lap(person[0], fastest_lap))
 
-print('\nSektorite parimad')
-total_sector_time = sum(sector[1] for sector in three_sectors)
-for idx, (driver, sector_time) in enumerate(three_sectors):
-    print(f"Sektor {idx+1}", driver.ljust(10), format_time(sector_time))
-print("Unelmate ring", format_time(total_sector_time))
+    print('Sektorite parimad')
+    total = 0
+    for idx, driver in enumerate(three_sectors):
+        total += driver[1]
+        print('Sektor', (idx + 1), driver[0].ljust(10), sec2time(driver[1]))
+    print('Unelmate ring', sec2time(total))
